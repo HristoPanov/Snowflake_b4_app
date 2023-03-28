11efc3fef40ad67f296713e6243987157aa24331
HristoPanov <hristopanov@gmail.com>
2023-03-28 12:38:20 +0300

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -1,7 +1,5 @@
 import streamlit
 import pandas as pd
-import requests
 import snowflake.connector
-from urllib.error import URLError
 
-streamlit.title('Zenas Catalog')
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
